db9ded887a482ac527ea0294586d781e5dc662b6
unolife <unolife92@gmail.com>
2022-03-19 10:47:03 +0900

    example data

--- server/inputModel.py
+++ server/inputModel.py
@@ -1,4 +1,23 @@
 from pydantic import BaseModel
 
 class InputData(BaseModel):
-    name: str
+    term: str
+    int_rate: float
+    grade: str
+    emp_length: str
+    home_ownership: str
+    annual_inc: int
+    verification_status: str
+    purpose: str
+    dti: float
+    inq_last_6mths: int
+    revol_util: float
+    total_acc: int
+    total_rec_int: float
+    last_pymnt_amnt: str
+    tot_cur_bal: str
+    total_rev_hi_lim: str
+    earliest_cr_line: str
+    issue_d: str
+    last_pymnt_d: str
+    last_credit_pull_d: str
